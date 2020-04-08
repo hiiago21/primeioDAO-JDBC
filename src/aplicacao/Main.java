537a489bc33cdfa91ea5b94f92e0367faa234579
Hiago Silva <hiiago@yahoo.com.br>
2020-04-08 06:37:25 -0300

    Criado a classe Departamento

---
+++ src/aplicacao/Main.java
@@ -0,0 +1,15 @@
+package aplicacao;
+
+import model.entidades.Departamento;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Departamento dp1 = new Departamento(1,"Books");
+		
+		System.out.println(dp1);
+
+	}
+
+}
